c88bf789be7743c61aefa738e8cd6ecdb8e8206d
NataliyaChe <happylibrarygeek@gmail.com>
2023-01-10 23:04:43 +0200

    router to ver.6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-// import Header from './components/Header'
-// import Main from './components/Main'
+import {BrowserRouter,  Routes, Route, Link} from 'react-router-dom';
+import Header from './components/Header'
+import Main from './components/Main'
 import Login from './components/Login'
 
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='navbar'>
+      {/* <div className='navbar'>
         <div className='navbar-links'>
           <a href='/login'>Login</a>
           <a href='/main'>Main</a>
         </div>
-      </div>
-      {/* <Route path='/login'>
-        <Login/>
-      </Route> */}
-      {/* <Route path='/main'>
-        <Main/>
-      </Route> */}
+      </div> */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        {/* <Route path='/' element={<Login />} /> */}
+        
+        
+      </Routes>
       Main page
     </BrowserRouter>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
-    <header className='header'>
-        <h1 className='header__title'>Animelist</h1>
-    </header>
+    <div className='navbar'>
+      <div className='navbar__links'>
+        <Link to='.'>Main</Link>
+        <Link to='login'>Login</Link>
+        
+      </div>
+    </div>
   );
 }
 
