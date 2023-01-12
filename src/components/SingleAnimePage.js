a7a4f92f379318dce8ed231c8693527ab6c3d483
NataliyaChe <happylibrarygeek@gmail.com>
2023-01-12 02:59:25 +0200

    fetching by id added

--- src/components/SingleAnimePage.js
+++ src/components/SingleAnimePage.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 
 
 function SingleAnimePage(props) {
     const params = useParams();
     console.log('params', params);
+
+    const [anime, setSingleAnime] = useState();
+
+    useEffect(() => {
+        const fetchSingleAnime = async () => {
+          const data = await fetch('https://api.jikan.moe/v4/top/anime/{params.mal_id}');
+          const singleAnime = await data.json();
+          setSingleAnime(anime.data)
+        }
+        fetchSingleAnime()
+      }, []);
+
     return (
         <div className='single-anime'>
             <h2>Single anime page</h2>
