722c8c1492c7b8bb105cc41fb83b3d50d605b794
NataliyaChe <happylibrarygeek@gmail.com>
2023-02-03 23:21:58 +0200

    styles fixed

--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
-const styles = {
-    div: {
-        padding: '10px',
-        border: '2px solid teal',
-        borderRadius: '10px',
-        maxWidth: '150px',
-    }
-}
-
 function AnimeItem({ anime }) {
   let navigate = useNavigate();
 
   return (
     <div className="anime-item" 
-      style={styles.div} 
       onClick={() => navigate(`/${anime.mal_id}`)}>
       <img className="anime-poster" 
         src={anime.images.webp.image_url} 
